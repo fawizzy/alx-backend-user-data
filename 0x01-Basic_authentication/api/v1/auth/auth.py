f03b0c80f9d2512b83b117a62b0d2433e763bacd
Fawaz Oduola <oduolafawaz@gmail.com>
2023-05-03 01:27:03 +0100

    task auth started

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,21 @@ class Auth():
         '''
             Return false
         '''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        
+        if path in excluded_paths:
+            return False
+        
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                if path == excluded_path:
+                    return False
+            if excluded_path.startswith(path):
+                return False
+        return True
     
 
     def authorization_header(self, request=None) -> str: 
