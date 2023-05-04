774363d03af5b15c63e210d609a768f86c3cf45d
Fawaz Oduola <oduolafawaz@gmail.com>
2023-05-04 17:02:09 +0100

    checking task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ Basic auth class
 '''
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -61,3 +62,23 @@ class BasicAuth(Auth):
         password = decoded_base64_authorization_header.split(':')[1]
 
         return email, password
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+            ) -> TypeVar('User'):
+        '''
+        get user object from credentials
+        '''
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({"email": user_email})
+        if not users or users == []:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
